b446d0a96f4d3cc35f9e22badc051178e96d0f01
maaaaaaaaad <integral5137@gmail.com>
2025-05-08 15:02:29 +0900

    e

--- pointer_void.c
+++ pointer_void.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
+    const int arr[3] = {0};
+    const float f = 0.0f;
+
+    const void *p1 = arr;
+    const void *p2 = &f;
+
+    printf("p1: %p\n", p1);
+    printf("p2: %p\n", p2);
+
     return 0;
 }
