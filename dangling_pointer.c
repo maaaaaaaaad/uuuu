2056e7829ce90f03134b9c72c32c3e0816272007
maaaaaaaaad <integral5137@gmail.com>
2025-05-12 17:46:28 +0900

    e

--- dangling_pointer.c
+++ dangling_pointer.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+int *dangling_pointer(void) {
+    int n = 30;
+    return &n;
+}
+
 int main(void) {
+    int *p = dangling_pointer();
+
+    printf("p: %p\n", p);
+
+    p = NULL;
     return 0;
 }
