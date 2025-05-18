47529f9d8400c5de4589d7a48f77257c400551a8
maaaaaaaaad <integral5137@gmail.com>
2025-05-18 21:55:39 +0900

    e

--- pointer_function_01.c
+++ pointer_function_01.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 
-char copyValues(char x, char y) {
+void copyValues(char x, char y) {
     const char tmp = x;
     x = y;
     y = tmp;
+}
 
-    return 0;
+void ptrValues(char *x, char *y) {
+    const char tmp = *x;
+    *x = *y;
+    *y = tmp;
 }
 
 int main() {
-    const char x = 5;
-    const char y = 10;
+    char x = 5;
+    char y = 10;
     copyValues(x, y);
-    printf("x: %d, y: %d\n", x, y);
+    printf("copyValues = x: %d, y: %d\n", x, y);
+
+
+    ptrValues(&x, &y);
+    printf("ptrValues = x: %d, y: %d\n", x, y);
 
     return 0;
 }
