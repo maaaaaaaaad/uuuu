970b869755f6089109a7c4bb406896670253f10b
maaaaaaaaad <integral5137@gmail.com>
2025-05-25 20:59:14 +0900

    e

--- malloc_02.c
+++ malloc_02.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void add(char *str);
 
@@ -9,5 +11,24 @@ int len = 2;
 int pos = 0;
 
 int main(void) {
+    char input[50];
+    const char intputLength = sizeof(input);
+
+    strArr = malloc(len * sizeof(char *));
+
+    printf("Enter strings: ");
+    printf(">");
+
+    while (strlen(fgets(input, intputLength, stdin))) {
+        add(input);
+        printf(">");
+    }
+
+    for (int i = 0; i < pos; i++) {
+        free(strArr[i]);
+    }
+
+    free(strArr);
+
     return 0;
 }
