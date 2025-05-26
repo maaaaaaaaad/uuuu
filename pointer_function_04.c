2a73cdb54c1d403c47268fc085577f84b451136b
maaaaaaaaad <integral5137@gmail.com>
2025-05-26 20:26:39 +0900

    e

--- pointer_function_04.c
+++ pointer_function_04.c
@@ -10,5 +10,13 @@ int minus(const int a, const int b) {
 }
 
 int main(void) {
+    int (*pFunc[])(int, int) = {add, minus};
+    const int pFuncLength = sizeof(pFunc) / sizeof(pFunc[0]);
+
+    for (int i = 0; i < pFuncLength; i++) {
+        printf("pFunc[%d]=%p, result: ", i, pFunc[i]);
+        printf("%d\n", pFunc[i](1, 2));
+    }
+
     return 0;
 }
