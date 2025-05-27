c4a68524cf2f01e92122e96228463608bce54bd9
maaaaaaaaad <integral5137@gmail.com>
2025-05-27 19:59:35 +0900

    e

--- struct_01.c
+++ struct_01.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
+typedef struct commonAttributes {
+    int createdAt;
+    int updatedAt;
+    int deletedAt;
+} CommonAttributes;
+
 typedef struct member {
     char id[10];
     char name[5];
     int8_t age;
+    CommonAttributes commonAttributes;
 } Member;
 
 int main(void) {
     Member member = {0};
 
     printf("&member: %p\n", &member);
+    printf("sizeof(member): %lu bytes\n", sizeof(member));
     return 0;
 }
