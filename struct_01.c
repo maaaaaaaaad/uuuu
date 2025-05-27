190a0c4f6e25c78120e08fabaadaa73e92e36474
maaaaaaaaad <integral5137@gmail.com>
2025-05-27 19:52:34 +0900

    e

--- struct_01.c
+++ struct_01.c
@@ -3,10 +3,11 @@
 struct member {
     char id[10];
     char name[5];
+    int8_t age;
 };
 
 int main(void) {
-    struct member member;
+    struct member member = {0};
 
     printf("&member: %p\n", &member);
     return 0;
