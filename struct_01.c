64d67e789fa2cad021ae33fc4e0429e5b551eeb4
maaaaaaaaad <integral5137@gmail.com>
2025-05-29 21:55:14 +0900

    e

--- struct_01.c
+++ struct_01.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+typedef struct {
+    int date;
+} Abstract;
+
+typedef struct {
+    int age;
+    char name[10];
+    Abstract abstract;
+} Member;
 
 int main(void) {
     return 0;
