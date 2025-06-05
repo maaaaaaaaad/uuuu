90222f75ae8e80f00b57cf40f1e2ceb292d6219f
maaaaaaaaad <integral5137@gmail.com>
2025-06-05 15:00:57 +0900

    e

--- main.c
+++ main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
+void sum(int *arr, const char *arrLength) {
+    for (char i = 0; i < *arrLength; i++) {
+        arr[i] = i + 1;
+    }
+}
+
 int main(void) {
     int arr[3] = {0};
     const char arrLength = sizeof(arr) / sizeof(arr[0]);
 
+    sum(&arr[0], &arrLength);
     for (char i = 0; i < arrLength; i++) {
-        arr[i] = i + 1;
         printf("%d\n", arr[i]);
     }
     return 0;
