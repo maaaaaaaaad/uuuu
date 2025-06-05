3204dccdb776cf7b046aa0b599215a8c523373bd
maaaaaaaaad <integral5137@gmail.com>
2025-06-05 14:57:52 +0900

    e

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 int main(void) {
     int arr[3] = {0};
     const char arrLength = sizeof(arr) / sizeof(arr[0]);
-    printf("%d\n", arrLength);
+
+    for (char i = 0; i < arrLength; i++) {
+        arr[i] = i + 1;
+        printf("%d\n", arr[i]);
+    }
     return 0;
 }
