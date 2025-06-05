7b4839ab6eb7a6a97d62d5a4f0ace615fd6b73f5
maaaaaaaaad <integral5137@gmail.com>
2025-06-05 14:56:23 +0900

    e

--- main.c
+++ main.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-    const int arr[3] = {0};
-    const int arr_length = sizeof(arr) / sizeof(arr[0]);
-
-    int *arr_ptr = arr;
-    arr_ptr++;
-    *arr_ptr = 1;
-
-    for (int i = 0; i < arr_length; i++) {
-        printf("%d ", arr[i]);
-    }
-
+    int arr[3] = {0};
+    const char arrLength = sizeof(arr) / sizeof(arr[0]);
+    printf("%d\n", arrLength);
     return 0;
 }
