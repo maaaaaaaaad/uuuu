abe5a8b3217862561ffc08b592321bf2619aa690
maaaaaaaaad <integral5137@gmail.com>
2025-06-06 21:29:09 +0900

    e

--- main.c
+++ main.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #define MAX_ARRAY_LENGTH 100
 
-void sum(int *arr, const char *arrLength, int *sumNumber) {
-    for (char i = 0; i < *arrLength; i++) {
-        arr[i] = i + 1;
-        *sumNumber += arr[i];
-    }
+void sum(const char *arrLength, int *sumNumber) {
+    *sumNumber = (*arrLength) * (*arrLength + 1) / 2;
 }
 
 int main(void) {
-    int arr[MAX_ARRAY_LENGTH] = {0};
     int sumArr = 0;
     const char arrLength = MAX_ARRAY_LENGTH;
 
-    sum(&arr[0], &arrLength, &sumArr);
+    sum(&arrLength, &sumArr);
 
     printf("sumArr: %d\n", sumArr);
     return 0;
