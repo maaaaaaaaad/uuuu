d6c44b5c636eb96d34bc262872cd53e07b4daaab
maaaaaaaaad <integral5137@gmail.com>
2025-06-10 16:04:08 +0900

    e

--- sum.c
+++ sum.c
@@ -1,39 +1,7 @@
 #include <stdio.h>
 #define MAX_ARRAY_LENGTH 100
 
-int sumSequential(int *sumResult)
-{
-    const int arrLength = MAX_ARRAY_LENGTH;
-    return *sumResult = (arrLength * (arrLength + 1)) / 2;
-}
-
-int sumFor(int *arrArgs, int *sumResult)
-{
-    for (int i = 0; i < MAX_ARRAY_LENGTH; i++)
-    {
-        arrArgs[i] = i + 1;
-        *sumResult += arrArgs[i];
-    }
-    return *sumResult;
-}
-
-int sumDoWhile(int *arrArgs, int *sumResult)
-{
-    int i = 0;
-    do
-    {
-        arrArgs[i] = i + 1;
-        *sumResult += arrArgs[i];
-        i++;
-    } while (i < MAX_ARRAY_LENGTH);
-    return *sumResult;
-}
-
 int main(void)
 {
-    int *arr[MAX_ARRAY_LENGTH] = {0};
-    int sumResult = 0;
-    sumSequential(&sumResult);
-    printf("sumSequential: %d\n", sumResult);
     return 0;
 }
