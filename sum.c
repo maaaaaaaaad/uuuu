b18e5764ed97c8bf1aa83739bf3d2ebdc6159553
maaaaaaaaad <integral5137@gmail.com>
2025-06-11 15:07:03 +0900

    e

--- sum.c
+++ sum.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #define MAX_ARRAY_LENGTH 100
 
+void sumFor(const char *arrFirstIndex, int *sumResult)
+{
+    for (int i = 0; i < MAX_ARRAY_LENGTH; i++)
+    {
+        *sumResult += arrFirstIndex[i];
+    }
+}
+
 int main(void)
 {
     return 0;
