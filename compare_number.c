fabd6ecf294da92c76e27fc5fa1bbdd07878eeef
maaaaaaaaad <integral5137@gmail.com>
2025-06-15 20:24:45 +0900

    e

--- compare_number.c
+++ compare_number.c
@@ -1,20 +1,40 @@
 #include<stdio.h>
 
+int partition(int *arr, const int low, const int high)
+{
+    const int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            const int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    const int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    return (i + 1);
+}
+
+void quickSort(int *arr, const int low, const int high)
+{
+    if (low < high)
+    {
+        const int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
 int main(void)
 {
     int arr[4] = {3, 2, 1, 4};
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            const int tmp = arr[j];
-            if (arr[j] > arr[j + 1])
-            {
-                arr[j] = arr[j + 1];
-                arr[j + 1] = tmp;
-            }
-        }
-    }
+    quickSort(&arr[0], 0, 3);
 
     for (int i = 0; i < 4; i++)
     {
