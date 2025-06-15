f537e58b7f7ea0a521d6655f01552d387861cc81
maaaaaaaaad <integral5137@gmail.com>
2025-06-15 20:15:05 +0900

    e

---
+++ compare_number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int arr[4] = {3, 2, 1, 4};
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            const int tmp = arr[j];
+            if (arr[j] > arr[j + 1])
+            {
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
