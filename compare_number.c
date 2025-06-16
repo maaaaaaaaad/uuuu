df4e83e3bcde724461226642d3f4851e97ac0358
maaaaaaaaad <integral5137@gmail.com>
2025-06-17 07:28:13 +0900

    e

--- compare_number.c
+++ compare_number.c
@@ -1,14 +1,22 @@
 #include<stdio.h>
 #define MAX_ARRAY_LENGTH 100
 
-int main(void)
+void setArrayList(int *arrPtr)
 {
-    int arr[MAX_ARRAY_LENGTH] = {0};
-    int *arrPtr = &arr[0];
     for (int i = 0; i < MAX_ARRAY_LENGTH; i++)
     {
         *arrPtr = i + 1;
         *arrPtr++;
+    }
+}
+
+int main(void)
+{
+    int arr[MAX_ARRAY_LENGTH] = {0};
+    setArrayList(&arr[0]);
+    for (int i = 0; i < MAX_ARRAY_LENGTH; i++)
+    {
+        printf("%d\n", arr[i]);
     }
     return 0;
 }
