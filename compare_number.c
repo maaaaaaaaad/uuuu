9fd26f99f86f997b2c14741fc5493e556c8657e4
maaaaaaaaad <integral5137@gmail.com>
2025-06-16 21:55:34 +0900

    e

--- compare_number.c
+++ compare_number.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
-
+#define MAX_ARRAY_LENGTH 100
 
 int main(void)
 {
+    int arr[MAX_ARRAY_LENGTH] = {0};
+    int *arrPtr = &arr[0];
+    for (int i = 0; i < MAX_ARRAY_LENGTH; i++)
+    {
+        *arrPtr = i + 1;
+        *arrPtr++;
+    }
     return 0;
 }
