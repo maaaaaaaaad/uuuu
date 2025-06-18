67b58feb64d4d52db659797a4b296926b5e2d561
maaaaaaaaad <integral5137@gmail.com>
2025-06-18 16:34:48 +0900

    e

--- main.c
+++ main.c
@@ -2,54 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int get_array_size()
-{
-    char buffer[32];
-    int size;
-
-    while (1)
-    {
-        printf("(1-1000000): ");
-
-        if (fgets(buffer, sizeof(buffer), stdin) == NULL)
-        {
-            continue;
-        }
-
-        size_t len = strlen(buffer);
-        if (len > 0 && buffer[len - 1] == '\n')
-        {
-            buffer[len - 1] = '\0';
-        }
-
-        if (sscanf(buffer, "%d", &size) == 1 && size > 0 && size <= 1000000)
-        {
-            return size;
-        }
-    }
-}
-
 int main(void)
 {
-    const int n = get_array_size();
-
-    int *array = (int *) malloc(n * sizeof(int));
-    if (array == NULL)
-    {
-        return 1;
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        array[i] = i;
-    }
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d", array[i]);
-        if (i < n - 1) printf(", ");
-    }
-
-    free(array);
-
     return 0;
 }
