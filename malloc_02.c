e913d2f434afd333f123b954ee5b735d5162b97f
maaaaaaaaad <integral5137@gmail.com>
2025-06-20 17:45:00 +0900

    e

--- malloc_02.c
+++ malloc_02.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX_ARRAY_LENGTH 100
+#define safeFree(p) if(p) {free(p); p = NULL;}
 
 int main(void)
 {
+    int *score = (int *) malloc(sizeof(int) * MAX_ARRAY_LENGTH);
+
+    for (int i = 0; i < MAX_ARRAY_LENGTH; i++)
+    {
+        score[i] = i + 1;
+        printf("%d\n", score[i]);
+    }
+
+    safeFree(score);
     return 0;
 }
