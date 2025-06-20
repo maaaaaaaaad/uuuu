a2a534330a96c0256149808df88f0ba01bb720cb
maaaaaaaaad <integral5137@gmail.com>
2025-06-20 17:41:30 +0900

    e

--- malloc_02.c
+++ malloc_02.c
@@ -1,69 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-void add(const char *str);
-
-void printArr(void);
-
-char **strArr;
-int len = 2;
-int pos = 0;
 
 int main(void)
 {
-    char input[50];
-    const char intputLength = sizeof(input);
-
-    strArr = malloc(len * sizeof(char *));
-
-    printf("Enter strings: ");
-    printf(">");
-
-    while (strlen(fgets(input, intputLength, stdin)))
-    {
-        add(input);
-        printf(">");
-    }
-
-    for (int i = 0; i < pos; i++)
-    {
-        free(strArr[i]);
-    }
-
-    free(strArr);
-
     return 0;
-}
-
-void add(const char *str)
-{
-    char *tmp = malloc(sizeof(char) * strlen(str) + 1);
-
-    if (pos >= len)
-    {
-        char **newStarArr = malloc(sizeof(char *) * len * 2);
-        memcpy(newStarArr, strArr, len * sizeof(char *));
-        free(strArr);
-        strArr = newStarArr;
-        len *= 2;
-    }
-
-    strcpy(tmp, str);
-    strArr[pos++] = tmp;
-
-    printArr();
-}
-
-void printArr(void)
-{
-    int i;
-    printf("strArr=%p: [", strArr);
-
-    for (int j = 0; j < pos; j++)
-    {
-        printf("%s", strArr[j]);
-    }
-
-    printf("] len=%d\n", len);
 }
