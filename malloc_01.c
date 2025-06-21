875382a76177852498a1a457f2e7b29f93233dbe
maaaaaaaaad <integral5137@gmail.com>
2025-06-21 21:57:18 +0900

    e

--- malloc_01.c
+++ malloc_01.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define safeFree(p) if(p) {free(p); p = NULL;}
+#define fieldsSize 5
 
 int main(void)
 {
-    const char memSize = sizeof(char);
-    char *p = malloc(memSize);
-    safeFree(p);
+    int *scores = (int *) malloc(fieldsSize * sizeof(int));
+
+    for (int i = 0; i < fieldsSize; i++)
+    {
+        printf("Input field %d: ", i + 1);
+        scanf("%d", &scores[i]);
+    }
+
+    safeFree(scores);
     return 0;
 }
