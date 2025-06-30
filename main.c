a27e8e4b35e14375aeb7c46ee69d313bd164f2a6
maaaaaaaaad <integral5137@gmail.com>
2025-06-30 22:00:36 +0900

    e

--- main.c
+++ main.c
@@ -1,19 +1,4 @@
-#include <stdio.h>
-#define MAX_ARR_LENGTH 100
-
-void sumResult(const int *arrLength, int *result)
-{
-    *result = *arrLength * (*arrLength + 1) / 2;
-}
-
-
 int main(void)
 {
-    const int arrLength = MAX_ARR_LENGTH;
-    int result = 0;
-
-    sumResult(&arrLength, &result);
-
-    printf("result: %d\n", result);
     return 0;
 }
