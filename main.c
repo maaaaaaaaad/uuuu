9a8ed08c8745bf44a1f969802dc67364c8816916
maaaaaaaaad <integral5137@gmail.com>
2025-07-04 17:00:27 +0900

    e

--- main.c
+++ main.c
@@ -2,58 +2,5 @@
 
 int main()
 {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    const int n = sizeof(arr) / sizeof(arr[0]);
-
-    for (int i = n / 2 - 1; i >= 0; i--)
-    {
-        int k = i;
-        int v = arr[k];
-        bool heap = false;
-
-        while (!heap && 2 * k + 1 < n)
-        {
-            int j = 2 * k + 1;
-            if (j + 1 < n)
-                if (arr[j] < arr[j + 1])
-                    j = j + 1;
-            if (v >= arr[j])
-                heap = true;
-            else
-            {
-                arr[k] = arr[j];
-                k = j;
-            }
-        }
-        arr[k] = v;
-    }
-
-    for (int i = n - 1; i > 0; i--)
-    {
-        const int temp = arr[0];
-        arr[0] = arr[i];
-        arr[i] = temp;
-
-        int k = 0;
-        int v = arr[k];
-        bool heap = false;
-
-        while (!heap && 2 * k + 1 < i)
-        {
-            int j = 2 * k + 1;
-            if (j + 1 < i)
-                if (arr[j] < arr[j + 1])
-                    j = j + 1;
-            if (v >= arr[j])
-                heap = true;
-            else
-            {
-                arr[k] = arr[j];
-                k = j;
-            }
-        }
-        arr[k] = v;
-    }
-
     return 0;
 }
