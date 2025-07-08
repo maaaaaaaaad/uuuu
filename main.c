3b65e0f135b8cae1e06c0a399610013a4ed4ed7a
maaaaaaaaad <integral5137@gmail.com>
2025-07-08 17:00:38 +0900

    e

--- main.c
+++ main.c
@@ -7,15 +7,17 @@ int main()
 
     for (int i = 0; i < n - 1; i++)
     {
-        for (int j = 0; j < n - i - 1; j++)
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++)
         {
-            if (arr[j] > arr[j + 1])
+            if (arr[j] < arr[min_idx])
             {
-                const int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                min_idx = j;
             }
         }
+        const int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
     }
 
     return 0;
