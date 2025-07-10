7c2689d08230d81ed34dc0cd8e8c3472b4e5d509
maaaaaaaaad <integral5137@gmail.com>
2025-07-10 18:59:20 +0900

    e

--- main.c
+++ main.c
@@ -5,54 +5,47 @@ int main()
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     const int n = sizeof(arr) / sizeof(arr[0]);
 
-    for (int i = n / 2 - 1; i >= 0; i--)
+    int stack[32];
+    int top = -1;
+    stack[++top] = 0;
+    stack[++top] = n - 1;
+
+    while (top >= 0)
     {
-        int k = i;
-        const int v = arr[k];
-        bool heap = false;
+        const int right = stack[top--];
+        const int left = stack[top--];
 
-        while (!heap && 2 * k + 1 < n)
+        const int pivot = arr[right];
+        int i = left - 1;
+
+        for (int j = left; j < right; j++)
         {
-            int j = 2 * k + 1;
-            if (j + 1 < n && arr[j] < arr[j + 1])
-                j++;
-
-            if (v >= arr[j])
-                heap = true;
-            else
+            if (arr[j] <= pivot)
             {
-                arr[k] = arr[j];
-                k = j;
+                i++;
+                const int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
-        arr[k] = v;
-    }
 
-    for (int i = n - 1; i > 0; i--)
-    {
-        const int temp = arr[0];
-        arr[0] = arr[i];
-        arr[i] = temp;
+        const int temp = arr[i + 1];
+        arr[i + 1] = arr[right];
+        arr[right] = temp;
 
-        int k = 0;
-        const int v = arr[k];
-        bool heap = false;
+        const int partition = i + 1;
 
-        while (!heap && 2 * k + 1 < i)
+        if (partition - 1 > left)
         {
-            int j = 2 * k + 1;
-            if (j + 1 < i && arr[j] < arr[j + 1])
-                j++;
-
-            if (v >= arr[j])
-                heap = true;
-            else
-            {
-                arr[k] = arr[j];
-                k = j;
-            }
+            stack[++top] = left;
+            stack[++top] = partition - 1;
         }
-        arr[k] = v;
+
+        if (partition + 1 < right)
+        {
+            stack[++top] = partition + 1;
+            stack[++top] = right;
+        }
     }
 
     return 0;
