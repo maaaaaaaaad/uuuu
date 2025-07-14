e1f1992c17969e52ca8110e6af63e4ef03e7a14f
maaaaaaaaad <integral5137@gmail.com>
2025-07-14 17:02:12 +0900

    e

--- main.c
+++ main.c
@@ -1,50 +1,24 @@
-#include <stdbool.h>
+#include <stdio.h>
 
 int main()
 {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    const int n = sizeof(arr) / sizeof(arr[0]);
+    const int size = 7;
+    int hash[100] = {0};
 
-    int stack[32];
-    int top = -1;
-    stack[++top] = 0;
-    stack[++top] = n - 1;
-
-    while (top >= 0)
+    for (int i = 0; i < size; i++)
     {
-        const int right = stack[top--];
-        const int left = stack[top--];
+        const int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        hash[arr[i]]++;
+    }
 
-        const int pivot = arr[right];
-        int i = left - 1;
-
-        for (int j = left; j < right; j++)
+    for (int i = 0; i < 100; i++)
+    {
+        if (hash[i] > 0)
         {
-            if (arr[j] <= pivot)
+            for (int j = 0; j < hash[i]; j++)
             {
-                i++;
-                const int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                printf("%d ", i);
             }
-        }
-
-        const int temp = arr[i + 1];
-        arr[i + 1] = arr[right];
-        arr[right] = temp;
-
-        const int partition = i + 1;
-
-        if (partition - 1 > left)
-        {
-            stack[++top] = left;
-            stack[++top] = partition - 1;
-        }
-
-        if (partition + 1 < right)
-        {
-            stack[++top] = partition + 1;
-            stack[++top] = right;
         }
     }
 
