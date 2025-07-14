3e05e2bcb735492d02a8ef9b4e4b096b18c8941b
maaaaaaaaad <integral5137@gmail.com>
2025-07-14 21:41:43 +0900

    e

--- main.c
+++ main.c
@@ -3,21 +3,40 @@
 int main()
 {
     const int size = 7;
-    int hash[100] = {0};
+    const int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int max_element = arr[0];
+    int min_element = arr[0];
+
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] > max_element)
+        {
+            max_element = arr[i];
+        }
+        if (arr[i] < min_element)
+        {
+            min_element = arr[i];
+        }
+    }
+
+    int hash[max_element - min_element + 1];
+    for (int i = 0; i < max_element - min_element + 1; i++)
+    {
+        hash[i] = 0;
+    }
 
     for (int i = 0; i < size; i++)
     {
-        const int arr[] = {64, 34, 25, 12, 22, 11, 90};
-        hash[arr[i]]++;
+        hash[arr[i] - min_element]++;
     }
 
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < max_element - min_element + 1; i++)
     {
         if (hash[i] > 0)
         {
             for (int j = 0; j < hash[i]; j++)
             {
-                printf("%d ", i);
+                printf("%d ", i + min_element);
             }
         }
     }
