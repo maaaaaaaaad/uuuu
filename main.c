7e5c01b7144670ed2e9ebe592246fd66f63d1d8b
maaaaaaaaad <integral5137@gmail.com>
2025-08-03 16:40:59 +0900

    e

--- main.c
+++ main.c
@@ -1,75 +1,18 @@
-#include <stdio.h>
-#include <limits.h>
-
-#define V 9
-#define INF INT_MAX
-
-void printSolution(int dist[], int n)
+typedef struct
 {
-    printf("Vertex   Distance from Source\n");
-    for (int i = 0; i < n; i++)
-        printf("%d \t\t %d\n", i, dist[i]);
-}
+    int pid;
+    int burst_time;
+    int waiting_time;
+    int turnaround_time;
+} Process;
 
-int minDistance(const int dist[], const int sptSet[], int n)
+void findAverageTime(Process *process, int length)
 {
-    int min = INF, min_index = 0;
-
-    for (int v = 0; v < n; v++)
-        if (!sptSet[v] && dist[v] < min)
-        {
-            min = dist[v];
-            min_index = v;
-        }
-
-    return min_index;
-}
-
-void dijkstra(int graph[V][V], int src)
-{
-    int dist[V];
-    int sptSet[V] = {0};
-
-    for (int i = 0; i < V; i++)
-    {
-        dist[i] = INF;
-    }
-
-    dist[src] = 0;
-
-    for (int count = 0; count < V - 1; count++)
-    {
-        const int u = minDistance(dist, sptSet, V);
-        sptSet[u] = 1;
-
-        for (int v = 0; v < V; v++)
-        {
-            if (!sptSet[v] && graph[u][v] && dist[u] != INF &&
-                dist[u] + graph[u][v] < dist[v])
-            {
-                dist[v] = dist[u] + graph[u][v];
-            }
-        }
-    }
-
-    printSolution(dist, V);
+    process[0].waiting_time = 0;
+    process[0].turnaround_time = process[0].burst_time;
 }
 
 int main()
 {
-    int graph[V][V] = {
-        {0, 4, 0, 0, 0, 0, 0, 8, 0},
-        {4, 0, 8, 0, 0, 0, 0, 11, 0},
-        {0, 8, 0, 7, 0, 4, 0, 0, 2},
-        {0, 0, 7, 0, 9, 14, 0, 0, 0},
-        {0, 0, 0, 9, 0, 10, 0, 0, 0},
-        {0, 0, 4, 14, 10, 0, 2, 0, 0},
-        {0, 0, 0, 0, 0, 2, 0, 1, 6},
-        {8, 11, 0, 0, 0, 0, 1, 0, 7},
-        {0, 0, 2, 0, 0, 0, 6, 7, 0}
-    };
-
-    dijkstra(graph, 0);
-
     return 0;
 }
