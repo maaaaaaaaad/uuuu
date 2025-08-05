1b7b1371316e8a386e5d3517b04a1f120d458a5e
maaaaaaaaad <integral5137@gmail.com>
2025-08-05 18:21:25 +0900

    e

--- main.c
+++ main.c
@@ -1,62 +1,43 @@
 #include <stdio.h>
 
-typedef struct
+void swap(int *a, int *b)
 {
-    int pid;
-    int burst_time;
-    int waiting_time;
-    int turnaround_time;
-} Process;
+    const int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-void findAverageTime(Process *process, const int length)
+void selectionSort(int arr[], int n)
 {
-    for (int i = 0; i < length - 1; i++)
+    for (int i = 0; i < n - 1; i++)
     {
         int min_idx = i;
-        for (int j = i + 1; j < length; j++)
+        for (int j = i + 1; j < n; j++)
         {
-            if (process[j].burst_time < process[min_idx].burst_time)
+            if (arr[j] < arr[min_idx])
             {
                 min_idx = j;
             }
         }
         if (min_idx != i)
         {
-            const Process temp = process[i];
-            process[i] = process[min_idx];
-            process[min_idx] = temp;
+            swap(&arr[min_idx], &arr[i]);
         }
     }
-
-    process[0].waiting_time = 0;
-    process[0].turnaround_time = process[0].burst_time;
-
-    for (int i = 1; i < length; i++)
-    {
-        process[i].waiting_time = process[i - 1].waiting_time + process[i - 1].burst_time;
-        process[i].turnaround_time = process[i].waiting_time + process[i].burst_time;
-    }
-
-    float total_wt = 0;
-    float total_tat = 0;
-
-    printf("P_ID\tBurst\tWait\tTurnaround\n");
-    for (int i = 0; i < length; i++)
-    {
-        total_wt += (float) process[i].waiting_time;
-        total_tat += (float) process[i].turnaround_time;
-        printf("%d\t%d\t%d\t%d\n", process[i].pid, process[i].burst_time, process[i].waiting_time,
-               process[i].turnaround_time);
-    }
-
-    printf("Average waiting time = %.2f\n", total_wt / (float) length);
-    printf("Average turnaround time = %.2f\n", total_tat / (float) length);
 }
 
 int main()
 {
-    Process proc[] = {{1, 24}, {2, 3}, {3, 3}};
-    const int n = sizeof(proc) / sizeof(proc[0]);
-    findAverageTime(proc, n);
+    int arr[] = {64, 25, 12, 22, 11};
+    const int n = sizeof(arr) / sizeof(arr[0]);
+
+    selectionSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
     return 0;
 }
