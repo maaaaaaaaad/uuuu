9f0114a38390d1c833ac67026582944c28640715
maaaaaaaaad <integral5137@gmail.com>
2025-11-09 21:10:53 +0900

    e

--- main.c
+++ main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#define DEFAULT_VALUE 100
+
+unsigned int sum(unsigned int value);
 
 int main(void) {
-    const double x = 1.5;
-    const double y = 2.5;
-    const float z = 3.5f;
-    const double a = x + y + z;
-
-    printf("%f\n", a);
+    const unsigned int value = DEFAULT_VALUE;
+    printf("%u\n", sum(value));
     return 0;
+}
+
+unsigned int sum(const unsigned int value) {
+    unsigned int hap = 0;
+    for (unsigned int i = 1; i <= value; i++) {
+        hap = hap + i;
+    }
+    return hap;
 }
