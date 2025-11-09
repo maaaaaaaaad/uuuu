ed8acd53f6f3c80ed363a509dc86f8468366577b
maaaaaaaaad <integral5137@gmail.com>
2025-11-09 12:31:38 +0900

    e

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    const double x = 1.5;
+    const double y = 2.5;
+    const float z = 3.5f;
+    const double a = x + y + z;
+
+    printf("%f\n", a);
     return 0;
 }
